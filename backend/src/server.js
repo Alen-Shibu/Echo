0a806c73f3abd0a0189273220befb26c50f5bb8e
Alen Shibu <your-email@example.com>
2026-03-27 05:31:48 +0530

    created protectRoute middleware and check route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import "dotenv/config"
+import cookieParser from 'cookie-parser'
 
 import {connectDB} from './lib/db.js'
 import authRoutes from './routes/auth.routes.js'
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth",authRoutes)
